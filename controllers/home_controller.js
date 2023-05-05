22639cb20028ac285650e5e230e1a5b5f4e74fa0
vishalkjais <vishalk1665@gmail.com>
2023-05-05 21:48:48 +0530

    dfbg

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,9 +1,29 @@
+const Post=require('../models/post');
+
 module.exports.home=function(req,res){
     //return res.end('<h1>Express is up for Codeial!</h1>');
-    console.log(req.cookies);
-    res.cookie('user_id',250);
-    return res.render('home',{
-        title:"home"
-    });
-
+   // console.log(req.cookies);
+   // res.cookie('user_id',250);
+   // Post.find({},function(err,posts){
+     //       return res.render('home',{
+       //         title:"codeial | Home",
+         //       posts:posts
+           // });
+ //});
+ //poupulate the user of each post
+      Post.find({})
+      .populate('user')
+      .populate({
+        path:'comments',
+        populate:{
+          path:user
+        }
+      })
+      .exec(function(err,posts){
+        return res.render('home',{
+            title:"codeial | Home",
+            posts:posts
+        });
+      })
+    
 }
