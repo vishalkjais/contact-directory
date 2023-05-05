f8b7c0570b86dbad28b88ae228600d02d7e83552
vishalkjais <vishalk1665@gmail.com>
2023-05-05 21:49:01 +0530

    gnh

--- controllers/users_post.js
+++
@@ -1,5 +0,0 @@
-module.exports.post=function(req,res){
-
-    return res.end('<h1> ayu post </h1>');
-    
-}
--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const passport=require('passport');
  const passportLocal=require('./config/passport-local-strategy');
  const  MongoStore=require('connect-mongo')(session);
  //complete it after installing
- 
 
+ 
 app.use(express.urlencoded());
 
 app.use(cookieParser());
